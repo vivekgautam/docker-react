b5aa95a4c194c8ed11d5f2e866b4fab60cc0f117
vivek <gautamv27@gmail.com>
2020-01-29 23:14:10 -0500

    Feature branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> an save to reload.
+          Feature branch Change.
         </p>
       </div>
     );
